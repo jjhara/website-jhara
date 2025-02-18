4f4c94cd74f118d1d5af5322662fabdb21b3010c
jjhara <jhara@msp-lab.org>
2025-02-18 14:33:58 +0900

    metatitle2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'jhara's homepage',
+  title: 'jhara homepage',
   description: 'Description for My Stylish App',
   viewport: 'width=device-width, initial-scale=1', 
 }
